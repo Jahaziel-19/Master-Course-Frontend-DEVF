1210bec31f220efcd498f8beda8583aff8f2532c
Jahaziel-19 <jahazielvazquez19@gmail.com>
2026-04-22 00:50:24 -0600

    Reto de clase 1 agregado: Tipos de dato en JS

---
+++ Módulo 1/Introducción a JavaScript/tipos-de-dato.js
@@ -0,0 +1,21 @@
+// Datos Number
+console.log(typeof 22); 
+console.log(typeof 3.14);
+console.log(typeof -5);
+console.log(typeof 0);
+
+// Datos String
+console.log(typeof "Treinta y tres"); 
+console.log(typeof 'Hola mundo');
+
+// Datos Boolean
+console.log(typeof true); 
+console.log(typeof false);
+
+// Datos Undefined
+console.log(typeof undefined);
+
+// Datos Null
+console.log(typeof null); // Esto es un error en JavaScript, pero se considera un tipo de dato especial
+
+console.log(typeof FALSE);
